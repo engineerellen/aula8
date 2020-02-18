72ed7213c4bdb7f2f1da5504b1e39c944b58b68e
Ellen <ellenengineer@gmail.com>
2020-02-18 07:13:07 -0300

    Gabarito - Atividade

---
+++ Atividades/Atividades/AtividadeParaNota/clsPessoa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AtividadeParaNota
+{
+     public class clsPessoa
+    {
+        public int ID { get; set; }
+        public string Documento { get; set; }
+        public string Nome { get; set; }
+        public string Endereco { get; set; }
+
+
+    }
+}
