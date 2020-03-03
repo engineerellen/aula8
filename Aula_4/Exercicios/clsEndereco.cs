469694a34215b0afec6c650eb92d0c95fa3b4b99
Ellen <ellenengineer@gmail.com>
2020-03-02 21:39:37 -0300

    clsEndereco

--- Aula_4/Exercicios/clsEndereco.cs
+++ Aula_4/Exercicios/clsEndereco.cs
@@ -4,7 +4,13 @@ using System.Text;
 
 namespace Exercicios
 {
-    public class clsEndereco
+    public class clsEndereco:IEndereco
     {
+        public string TipoEndereco { get; set; }
+        public int ID { get; set; }
+        public string Cidade { get; set; }
+        public string Logradouro { get; set; }
+        public string Bairro { get; set; }
+        public string Complemento { get; set; }
     }
 }
