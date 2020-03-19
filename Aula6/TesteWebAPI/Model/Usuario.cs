53714d3d197aed17fab1fee2d8fc4507831538d4
Ellen <ellenengineer@gmail.com>
2020-03-19 20:34:44 -0300

    Aula 6

---
+++ Aula6/TesteWebAPI/Model/Usuario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Model
+{
+    public class Usuario
+    {
+        public int ID { get; set; }
+        public string Login { get; set; }
+        public string Senha { get; set; }
+
+        public Usuario(string login)
+        {
+            this.Login = login;
+        }
+    }
+}
