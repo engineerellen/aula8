ad81f496d35e636f9c44844abfda019e590b4951
Ellen <ellenengineer@gmail.com>
2020-03-30 22:10:29 -0300

    aula 7

---
+++ Aula7/Model/Produto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Model
+{
+    public class Produto
+    {
+        public int ID { get; set; }
+        public string Descricao { get; set; }
+        public double ValorUnitario { get; set; }
+        public string  Detalhe { get; set; }
+    }
+}
